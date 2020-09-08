8a0156d8fc91e04daf15c24bdfeba0dd0d4b6c40
Лукашенко Дмитрий Александрович <da.lukashenka@vebtech.by>
2020-09-08 08:04:06 +0300

    add navigation component

--- app/src/main/java/by/dmitriy/koinapp/MainActivity.kt
+++ app/src/main/java/by/dmitriy/koinapp/MainActivity.kt
@@ -2,10 +2,14 @@ package by.dmitriy.koinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findNavController(R.id.containerNavigation).navigate(R.id.start_navigation)
     }
 }
---
+++ app/src/main/java/by/dmitriy/koinapp/MainFragment.kt
@@ -0,0 +1,37 @@
+package by.dmitriy.koinapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    companion object {
+        private const val ARG_PARAM1 = "param1"
+        private const val ARG_PARAM2 = "param2"
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    private val param1 by lazy { arguments?.getString(ARG_PARAM1) }
+    private val param2 by lazy { arguments?.getString(ARG_PARAM2) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+}
